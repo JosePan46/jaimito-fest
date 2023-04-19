8b156d9fb403a7bd717acd7a2bf895b34744a01d
JosePan46 <josepan@uma.es>
2023-04-19 14:51:09 +0200

    Detalles finales

--- src/app/components/operativo/costes/costes.component.ts
+++ src/app/components/operativo/costes/costes.component.ts
@@ -15,7 +15,10 @@ export class CostesComponent {
   public barChartData: ChartConfiguration<'bar'>['data'] = {
     labels: [ 'Productos', 'Personal', 'Artistas', 'Producción', 'Publicidad', 'Logística', 'Otros' ],
     datasets: [
-      { data: [ 65000, 59000, 80000, 81000, 56000, 55000, 40000 ], label: 'Costes (€)' }
+      { data: [ 65000, 59000, 80000, 81000, 56000, 55000, 40000 ], label: 'Costes (€)',
+      backgroundColor: ['rgba(54, 162, 235, 0.2)'],
+      borderColor: ['rgb(54, 162, 235)'],
+        borderWidth: 1}
     ]
   };
 
